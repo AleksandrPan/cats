16530f017015fa2a076a987a1935f958cc347d8d
aleks <panin-a@inbox.ru>
2023-01-25 01:51:37 +0300

    new

---
+++ script/cats.js
@@ -0,0 +1,79 @@
+const cats = [{
+    "id": 1,
+    "name": "Оболтус",
+    "age": 12,
+    "rate": 10,
+    "img_link" : "https://i1.wallbox.ru/wallpapers/main2/201712/149000670158cfb2ad5891b0.86537950.jpg",
+    "description" : "Потому что оболтус",
+    "favourite": true
+},
+{
+    "id": 2,
+    "name": "Барса",
+    "age": 5,
+    "rate": 8,
+    "description": "Сокращение от Барселона",
+    "img_link": "https://kartinkin.net/uploads/posts/2022-03/1646781714_12-kartinkin-net-p-koti-krasivie-kartinki-16.jpg"
+},
+{
+    "id": 3,
+    "name": "Рыжий",
+    "age": 6,
+    "rate": 9,
+    "img_link": "https://avatars.mds.yandex.net/i?id=73b7dade46bed6fbef3e2853c8a874bf-5287696-images-thumbs&n=13",
+    "favourite": false
+},
+{
+    "id": 4,
+    "name": "Цыпа",
+    "age": 13,
+    "rate": 10,
+    "description": "По радословной имя должно начинаться на М, по этому Марцыпан, а любя Цыпа",
+    "img_link" : "https://kuda-spb.ru/uploads/7722cf01bab5c04f550d341c5879369e.jpg",
+    "favourite": true
+},
+{
+    "id": 5,
+    "name": "Соня",
+    "age": 2,
+    "rate": 9,
+    "img_link" : "https://vplate.ru/images/article/orig/2019/03/samyj-bolshoj-mejn-kun-v-mire.jpg",
+    "favourite": true
+},
+{
+    "id": 6,
+    "name": "Клеопатра",
+    "age": 5,
+    "rate": 10,
+    "description": "Сокращенно Клепа",
+    "img_link" : "https://vsegda-pomnim.com/uploads/posts/2022-01/1643143439_49-vsegda-pomnim-com-p-yegipetskaya-khramovaya-koshka-foto-50.jpg",
+    "favourite": false
+},
+{
+    "id": 7,
+    "name": "Мени",
+    "age": 2,
+    "rate": 10,
+    "description": "Друг Клепы",
+    "img_link" : "https://haski-mana.ru/wp-content/uploads/a/8/1/a81faa78ade35c4a862eae49c2f14477.jpeg",
+    "favourite": true
+},
+{
+    "id": 8,
+    "name": "DARK",
+    "age": 14,
+    "rate": 7,
+    "description": "По тому ЧЕРНЫЙ)",
+    "img_link" : "https://mobimg.b-cdn.net/v3/fetch/12/12ec899aa5a958d196f16c7e7ddef54c.jpeg"
+},
+{
+    "id": 9,
+    "name": "Пуша",
+    "age": 0.3,
+    "rate": 10,
+    "description": "По тому что пушистый)",
+    "favourite": true,
+    "img_link" : "https://i.pinimg.com/originals/c3/bc/8e/c3bc8e9a1042b1ee232ca5a899b054f5.jpg"
+
+}]
+
